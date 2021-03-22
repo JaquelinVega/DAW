2204a3f6b490f483c622ad603a280d68d60ac0a8
JaquelinVega <71902957+JaquelinVega@users.noreply.github.com>
2021-03-22 09:19:56 -0700

    Las Rutas Y Vistas De Laravel
    Ruteador

--- Artesania/routes/web.php
+++ Artesania/routes/web.php
@@ -14,5 +14,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('hola');
+});
+Route::get('/producto/{id}', function ($id) {
+    return view('verproducto')->with('id',$id);
+});
+Route::get('/contacto', function(){
+    $contacto='Maria Jaquelin';
+    $valores=10;
+    $color="#ccc";
+    return view('contacto')->with('nombre',$contacto)
+    ->with('fondo',$color)
+    ->with('valores',$valores);
 });
