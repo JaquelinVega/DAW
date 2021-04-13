3973bdea86ef2b458a911284d19def94dbea4dfe
JaquelinVega <71902957+JaquelinVega@users.noreply.github.com>
2021-04-13 12:30:22 -0600

    Plantillas
    Sistema De Plantillas De Blade

---
+++ Artesania/resources/views/admin/productos.blade.php
@@ -0,0 +1,9 @@
+@extends('admin.layouts.main')
+
+@section('contenido')
+<h2>Estoy en Productos</h2>
+@endsection
+
+@section('scripts')
+<scrip> console.log('ajhaskjj') </script>
+@endsection
---
+++ Artesania/resources/views/admin/users.blade.php
@@ -0,0 +1,9 @@
+@extends('admin.layouts.main')
+
+@section('contenido')
+<h2>Estoy en usuarios</h2>
+@endsection
+
+@section('scripts')
+<scrip> console.log('ajhaskjj') </script>
+@endsection
